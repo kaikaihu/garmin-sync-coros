c46d6f0cd8a82c4cf40cb8b52f851004256d83af
KAIKAIHU <65703414+kaikaihu@users.noreply.github.com>
2026-08-10 18:17:40 +0800

    test: cover current garth token serialization

--- tests/test_garmin_global_token_bootstrap.py
+++ tests/test_garmin_global_token_bootstrap.py
@@ -14,6 +14,19 @@ class FakeGarth:
         return "serialized-token"
 
 
+class ClientStyleFakeGarth:
+    class Client:
+        def dumps(self):
+            return "client-serialized-token"
+
+    def __init__(self):
+        self.login_calls = []
+        self.client = self.Client()
+
+    def login(self, email, password):
+        self.login_calls.append((email, password))
+
+
 class GarminGlobalTokenBootstrapTests(unittest.TestCase):
     def test_create_token_logs_in_once_and_returns_serialized_garth_session(self):
         garth = FakeGarth()
@@ -21,6 +34,14 @@ class GarminGlobalTokenBootstrapTests(unittest.TestCase):
         token = create_token("user@example.com", "password", garth_client=garth)
 
         self.assertEqual(token, "serialized-token")
+        self.assertEqual(garth.login_calls, [("user@example.com", "password")])
+
+    def test_create_token_uses_client_serializer_when_newer_garth_has_no_module_serializer(self):
+        garth = ClientStyleFakeGarth()
+
+        token = create_token("user@example.com", "password", garth_client=garth)
+
+        self.assertEqual(token, "client-serialized-token")
         self.assertEqual(garth.login_calls, [("user@example.com", "password")])
 
 
